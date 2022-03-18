f99c71fc19b2cb226c3b7999c6c2705e3a2b3221
Lasse Nielsen <charmixer@users.noreply.github.com>
2022-03-18 11:13:35 +0100

    Adds validation for both application struct and cmd

--- cmd/root.go
+++ cmd/root.go
@@ -8,39 +8,81 @@ import (
 	"gopkg.in/yaml.v2"
 
 	"github.com/creasty/defaults"
+	"github.com/go-playground/validator"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
 	"github.com/charmixer/envconfig"
 	"github.com/charmixer/go-flags"
+	"github.com/charmixer/golang-cli-template/validation"
 )
 
 type App struct {
-	Description string `long:"app-description" description:"Description of application" default:"Gives a simple blueprint for creating new api's"`
+	Description string `long:"app-description" description:"Description of application" default:"Gives a simple blueprint for creating new cli based applications"`
 
 	Log struct {
 		Verbose bool   `long:"verbose" short:"v" description:"Verbose logging"`
 		Format  string `long:"log-format" description:"Logging format" choice:"json" choice:"plain"`
 	}
 
-	Version versionCmd `command:"version" description:"Prints the build information from the binary"`
+	Version versionCmd `command:"version" description:"Prints the build information from the binary" validate:"-"`
 }
 
 var Application App
 var parser = flags.NewParser(&Application, flags.HelpFlag|flags.PassDoubleDash)
 
 func Execute() {
-	_, err := parser.Execute()
 
-	if err != nil {
-		e := err.(*flags.Error)
-		if e.Type != flags.ErrCommandRequired && e.Type != flags.ErrHelp {
-			fmt.Printf("%s\n", e.Message)
+	// Validate Application options
+	// Using this required commands in struct to have validate:"-", otherwise each command will be validated everytime
+	err := validation.Validate.Struct(Application)
+
+	if err == nil {
+		// validate specific command options, when application has passed
+		cmd := parser.GetCommand()
+		if cmd != nil {
+			err = validation.Validate.Struct(cmd)
 		}
-		parser.WriteHelp(os.Stdout)
 	}
 
-	os.Exit(0)
+	if err != nil {
+		switch e := err.(type) {
+		case validator.ValidationErrors:
+			for _, verr := range e {
+				fmt.Printf("Validation failed for %s: %s\n", verr.StructNamespace(), verr.Translate(validation.Translation))
+			}
+		default:
+			fmt.Printf("%s\n", e.Error())
+		}
+
+		fmt.Println()
+		parser.WriteHelp(os.Stdout)
+		fmt.Println()
+
+		os.Exit(1)
+		return
+	}
+
+	_, err = parser.Execute()
+
+	retcode := 0
+	if err != nil {
+		switch e := err.(type) {
+		case *flags.Error:
+			if e.Type != flags.ErrCommandRequired && e.Type != flags.ErrHelp {
+				fmt.Printf("%s\n", e.Message)
+			}
+		case error:
+			fmt.Printf("%s\n", err.Error())
+			retcode = 1
+		}
+
+		fmt.Println()
+		parser.WriteHelp(os.Stdout)
+		fmt.Println()
+	}
+
+	os.Exit(retcode)
 }
 
 func init() {
